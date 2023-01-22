066b62df0d8bfe0946bae1fcf6e090f7b575dc46
Anastasia <naftya_malyshko@mail.ru>
2023-01-22 16:44:32 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0, num = number;
+while (number > 0) 
+{
+    i = i * 10 + number % 10; 
+    number /= 10;
+}
+if (num == i)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+Random rnd = new Random();
+
+int x1 = rnd.Next(-10, 11);
+int y1 = rnd.Next(-10, 11);
+int z1 = rnd.Next(-10, 11);
+int x2 = rnd.Next(-10, 11);
+int y2 = rnd.Next(-10, 11);
+int z2 = rnd.Next(-10, 11);
+Console.WriteLine($"x1 = {x1}, y1 = {y1}, z1 = {z1}, x2 = {x2}, y2 = {y2}, z2 = {z2}");
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine(result);
